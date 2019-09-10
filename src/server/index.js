a76034bd996d497537b877f1c64806cc43949f59
Philippe Haerens <haerphi@gmail.com>
2019-09-10 08:51:22 +0200

    modify index.js in server
    test route

--- src/server/index.js
+++ src/server/index.js
@@ -15,9 +15,43 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
-app.get("/hello", (req, res) => {
+app.get("/api/test", (req, res) => {
     console.log(`ℹ️  (${req.method.toUpperCase()}) ${req.url}`);
-    res.send("Hello, World!");
+    const rep = {
+        truc: [
+            {
+                _id: {$oid: "5393803ce0b8c05979c6ea65"},
+                bank: {$oid: "53937660e0b8c05979c6ea55"},
+                latitude: 51.1069,
+                longitude: 2.64824,
+                address: "Zeelaan 67, 8670 Koksijde",
+                created_at: "2013-06-10 18:43:44",
+                updated_at: "2013-06-10 18:43:44",
+                deleted_at: null,
+            },
+            {
+                _id: {$oid: "5393803ce0b8c05979c6ea65"},
+                bank: {$oid: "53937660e0b8c05979c6ea55"},
+                latitude: 51.1069,
+                longitude: 2.64824,
+                address: "Zeelaan 67, 8670 Koksijde",
+                created_at: "2013-06-10 18:43:44",
+                updated_at: "2013-06-10 18:43:44",
+                deleted_at: null,
+            },
+            {
+                _id: {$oid: "5393803ce0b8c05979c6ea65"},
+                bank: {$oid: "53937660e0b8c05979c6ea55"},
+                latitude: 51.1069,
+                longitude: 2.64824,
+                address: "Zeelaan 67, 8670 Koksijde",
+                created_at: "2013-06-10 18:43:44",
+                updated_at: "2013-06-10 18:43:44",
+                deleted_at: null,
+            },
+        ],
+    };
+    res.send(rep);
 });
 
 app.listen(APP_PORT, () =>
