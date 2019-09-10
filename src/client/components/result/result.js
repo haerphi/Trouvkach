0f8d20063f430982d618299338c00ed62e56d22a
Jonathan <atta.jon.t@gmail.com>
2019-09-10 10:52:26 +0200

    app refactored search deleted result as search page all components created

--- src/client/components/result/result.js
+++ src/client/components/result/result.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Map from "./map";
 import TerminalItem from "./terminal-item";
+import Search from "./search";
 
 export default function result() {
     return (
         <div>
+            <Search />
             <Map />
             <TerminalItem />
         </div>
