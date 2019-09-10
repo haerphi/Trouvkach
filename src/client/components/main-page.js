e9bcfd8e4207055199707f767bff941a62f661f2
Jonathan <atta.jon.t@gmail.com>
2019-09-10 10:44:15 +0200

    search deleted

--- src/client/components/main-page.js
+++ src/client/components/main-page.js
@@ -1,18 +1,15 @@
 import * as React from "react";
 
-import SearchCpn from "./search/search";
 import ResultCpn from "./result/result";
 import ItemCpn from "./item/item";
 
-const SearchPage = <SearchCpn />;
 const resultPage = <ResultCpn />;
 const itemPage = <ItemCpn />;
 
 const MainPage = () => (
     <div>
-        {SearchPage}
         {resultPage}
-        {itemPage}
+        {itemPage /*modal*/}
     </div>
 );
 
