d453b242f5989a06b5499c278cc9f33eed69d6df
Philippe Haerens <haerphi@gmail.com>
2019-09-12 19:09:09 +0200

    modify client/js/utils.js
    ajout de fonction pour récuperer les banques dans le client et les ajouter dans l'objet qui est affiché

--- src/client/js/utils.js
+++ src/client/js/utils.js
@@ -1,4 +1,43 @@
+//fonction pour récup les banques /api/search/banks/
+const getBanks = async () => {
+    const response = await fetch(`/api/search/banks/`, {
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+    });
+    const data = await response.json();
+
+    return data.truc;
+};
+
+const arrayObjectToString = arr => {
+    let newStr = "";
+    for (const element of arr) {
+        newStr += `${JSON.stringify(element)}|`;
+    }
+    newStr = newStr.substring(0, newStr.length - 1);
+    return newStr;
+};
+
+const stringToArrayObject = str => {
+    const newArr = str.split("|");
+    for (let i = 0; i < newArr.length; i++) {
+        newArr[i] = JSON.parse(newArr[i]);
+    }
+    return newArr;
+};
+
 exports.getTerminalAsync = async (long, lat, zoom) => {
+    let allbanks = localStorage.getItem("allbanks");
+    if (allbanks === null) {
+        allbanks = await getBanks();
+        localStorage.setItem("allbanks", arrayObjectToString(allbanks));
+    } else {
+        allbanks = stringToArrayObject(allbanks);
+    }
+
     const response = await fetch(`/api/search/${long}/${lat}/${zoom}`, {
         headers: {
             Accept: "application/json",
@@ -7,5 +46,13 @@ exports.getTerminalAsync = async (long, lat, zoom) => {
         method: "POST",
     });
     const data = await response.json();
+
+    //rajout des banks à l'emplacement nécessaire
+    for (let i = 0; i < data.truc.length; i++) {
+        //transformation en map où l'on met l'_id en avant et puis on recherche avec indexOf
+        const index = allbanks.map(e => e._id).indexOf(data.truc[i].bank);
+
+        data.truc[i].bank = allbanks[index];
+    }
     return data;
 };
