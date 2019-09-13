2931c9cebb822f31e9b01e26f5fe7be56f8d1cb2
Ashraf <ashraf@post.com>
2019-09-13 13:09:45 +0200

    Wrapped description in a paper (material ui)

--- src/client/components/result/description.js
+++ src/client/components/result/description.js
@@ -1,12 +1,18 @@
 import React from "react";
+import {Paper} from "@material-ui/core";
 
 export default function Description(props) {
     return (
-        <div>
-            {`Fucking Description!`}
-            <div>{props.obj.address && props.obj.address}</div>
-            <div>{props.obj.bank && props.obj.bank.name}</div>
-            <div>{props.obj.dist && `${props.obj.dist.calculated} m`}</div>
+        <div className={"item-description-container"}>
+            <Paper
+                style={{
+                    padding: "4vmin",
+                }}>
+                {`Fucking Description!`}
+                <div>{props.obj.address && props.obj.address}</div>
+                <div>{props.obj.bank && props.obj.bank.name}</div>
+                <div>{props.obj.dist && `${props.obj.dist.calculated} m`}</div>
+            </Paper>
         </div>
     );
 }
