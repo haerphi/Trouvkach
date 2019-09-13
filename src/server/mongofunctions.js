743dcb201e72b43fbcf31290a68df143299194da
Philippe Haerens <haerphi@gmail.com>
2019-09-13 13:16:11 +0200

    rename file installation to mongoinstallation

--- src/server/mongofunctions.js
+++ src/server/mongofunctions.js
@@ -1,48 +1,6 @@
 const mongo = require("mongodb").MongoClient;
 const url = "mongodb://dev:dev@mongo:27017";
 
-const updateBD = async () => {
-    console.log("UPDATE de la BD");
-    const client = await mongo.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    const db = client.db("trouvkash");
-    //récupération de la collection
-    const collection = db.collection("terminals");
-    const items = await collection.find({}).toArray();
-    for (const [i, element] of items.entries()) {
-        element.location = {
-            type: "Point",
-            coordinates: [element.longitude, element.latitude],
-        };
-        console.log(element);
-        console.log(i);
-        collection.updateOne({_id: element._id}, {$set: element});
-    }
-    await collection.createIndex({location: "2dsphere"});
-    client.close();
-};
-
-export const verify = async () => {
-    console.log("VERIFY de la BD");
-    const client = await mongo.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    const db = client.db("trouvkash");
-
-    const collection1 = db.collection("terminals");
-    const items = await collection1
-        .find({})
-        .limit(1)
-        .toArray();
-    if (!Object.prototype.hasOwnProperty.call(items[0], "location")) {
-        updateBD();
-    }
-    client.close();
-};
-
 export const mongoRequestBanks = async () => {
     const client = await mongo.connect(url, {
         useNewUrlParser: true,
