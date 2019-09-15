801b442b35426b3ebc4847fccff6aabaeab88beb
Ashraf <ashraf@post.com>
2019-09-15 10:05:21 +0200

    Styling item's description (working on)

--- src/client/components/result/description.js
+++ src/client/components/result/description.js
@@ -1,14 +1,19 @@
 import React from "react";
-import {Paper} from "@material-ui/core";
+import {Paper, Typography} from "@material-ui/core";
 
 export default function Description(props) {
     return (
         <div className={"item-description-container"}>
             <Paper
                 style={{
-                    padding: "4vmin",
+                    padding: "3vmin calc(1vmin + 0.5rem)",
                 }}>
-                {`Fucking Description!`}
+                <Typography
+                    style={{marginBottom: "20px", fontFamily: "Roboto-Regular"}}
+                    component={"p"}
+                    align={"center"}>
+                    {"Item's Description!"}
+                </Typography>
                 <div>{props.obj.address && props.obj.address}</div>
                 <div>{props.obj.bank && props.obj.bank.name}</div>
                 <div>{props.obj.dist && `${props.obj.dist.calculated} m`}</div>
