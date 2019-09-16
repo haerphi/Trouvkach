34325e7294986c0cc725a404333a7fdb2fd39350
Ashraf <ashraf@post.com>
2019-09-16 10:36:00 +0200

    Fixed bank color of undefined (to revert later)

--- src/client/components/result/terminal-ctnr/view-terminal.js
+++ src/client/components/result/terminal-ctnr/view-terminal.js
@@ -17,7 +17,7 @@ export default function viewTerminal(props) {
             onClick={setdesc}>
             <li
                 className={"item-property item-name"}
-                style={{color: `#${props.obj.bank.color}`}}>
+                style={{color: "#26a69a"}}>
                 {props.obj.bank ? props.obj.bank.name : `Unknow Bank`}
             </li>
             <li className={"item-property item-distance"}>
