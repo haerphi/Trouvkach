5faaab91fc793113872fb40939e8be5350c51df7
Jonathan <atta.jon.t@gmail.com>
2019-09-20 13:12:02 +0200

    Auto stash before merge of "functionalities" and "origin/master"

--- src/client/components/result/map.js
+++ src/client/components/result/map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Map, TileLayer, Marker, Popup} from "react-leaflet";
+import L, {Map, TileLayer, Marker, Popup} from "react-leaflet";
 
 export default function mapCtnr(props) {
     /*
@@ -27,40 +27,43 @@ export default function mapCtnr(props) {
     }
 
     return (
-        <div className={"leaflet-map-container"}>
-            <Map
-                id={"leaflet-map"}
-                center={[lat, lng]}
-                zoom={zoom}
-                maxZoom={19}
-                style={{
-                    height: "100%",
-                    width: "100%",
-                }}
-                attributionControl={false}
-                onZoom={horatioKayne}>
-                <TileLayer
-                    attribution={
-                        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    }
-                    url={"https://{s}.tile.osm.org/{z}/{x}/{y}.png"}
-                />
-                {AllAtm}
-                <Marker position={[lat, lng]}>
-                    <Popup>{"Your Position"}</Popup>
-                </Marker>
-                <Marker
-                    position={[props.onitemLatitude, props.onitemLongitude]}>
-                    <Popup>
-                        {`${props.obj.address} - ${(() => {
-                            if (typeof props.obj.bank != "undefined") {
-                                return props.obj.bank.name;
-                            }
-                            return "Unknow bank";
-                        })()}`}
-                    </Popup>
-                </Marker>
-            </Map>
-        </div>
+        <Map
+            id={"leaflet-map"}
+            center={[lat, lng]}
+            zoom={zoom}
+            maxZoom={19}
+            style={{
+                height: "calc(50vh - 64px)",
+                width: "100%",
+            }}
+            attributionControl={false}
+            onZoom={horatioKayne}>
+            <TileLayer
+                attribution={
+                    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                }
+                url={"https://{s}.tile.osm.org/{z}/{x}/{y}.png"}
+            />
+            {AllAtm}
+            <Marker position={[lat, lng]}>
+                <Popup>{"Your Position"}</Popup>
+            </Marker>
+            <Marker
+                position={[props.onitemLatitude, props.onitemLongitude]}
+                icon={
+                    new L.Icon({
+                        iconUrl: "./ppointer.svg",
+                    })
+                }>
+                <Popup>
+                    {`${props.obj.address} - ${(() => {
+                        if (typeof props.obj.bank != "undefined") {
+                            return props.obj.bank.name;
+                        }
+                        return "Unknow bank";
+                    })()}`}
+                </Popup>
+            </Marker>
+        </Map>
     );
 }
